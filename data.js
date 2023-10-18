8967cf49dddb0183f857c53a2eaf2ac353533a53
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-10-18 17:14:57 +0200

    first setup

---
+++ data.js
@@ -0,0 +1,41 @@
+const categories = [
+    {name: 'technology', color: '#cdb4db'},
+    {name: 'science', color: '#ffc8dd'},
+    {name: 'finance', color: '#ffafcc'},
+    {name: 'society', color: '#bde0fe'},
+    {name: 'entertainment', color: '#a2d2ff'},
+];
+
+
+const initialFacts = [
+    {
+        id: 1, 
+        text:'React is being developed by Meta (formerly facebook).',
+        source:'https://opensource.fb.com/',
+        category:'technology',
+        votesInteresting: 24,
+        votesMindblowing: 9,
+        votesFalse: 4,
+        createdIn: 2021
+    },
+    {
+        id: 2, 
+        text:'Astronauts grow taller in space!',
+        source:'https://www.spacecentre.co.uk/news/space-now-blog/why-do-you-grow-taller-in-space/',
+        category:'science',
+        votesInteresting: 8,
+        votesMindblowing: 3,
+        votesFalse: 0,
+        createdIn: 2020
+    },
+    {
+        id: 3, 
+        text:'We spend a year on the toilet in our lifetime.',
+        source:'https://dailyinfographic.com/your-bathroom-time',
+        category:'society',
+        votesInteresting: 15,
+        votesMindblowing: 96,
+        votesFalse: 1,
+        createdIn: 2019
+    },
+];
